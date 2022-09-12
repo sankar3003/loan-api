ebe19cd32175c3990efe89ccfb0dc52bb0a45652
sankar3003 <sankar30031997@gmail.com>
2022-09-12 20:21:00 +0530

    changes

--- routes/loan.js
+++ routes/loan.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const invoice= require("./../models/invoice.model")
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
